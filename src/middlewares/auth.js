b1f6a9148373fd69a9e3152bd6b2fbed84c3d187
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-11-01 11:15:38 +0700

    Auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,24 +6,24 @@ module.exports = {
     let token = req.headers.authorization
     token = token.split(' ')[1]
     jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
-      if (err) return helpers.response(res, null, { message: 'Token invalid' }, 403, 'Forbidden')
+      if (err) return helpers.response(res, { message: 'Token invalid' }, 403, 'Forbidden')
       next()
     })
-  },
-
-  verifyAccessSeller: (req, res, next) => {
-    let token = req.headers.authorization
-    token = token.split(' ')[1]
-    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
-      if (!err) {
-        if (decoded.roleId != 1) {
-          return helpers.response(res, null, { message: 'You are not a seller' }, 403, 'Forbidden')
-        } else {
-          next()
-        }
-      } else {
-        return helpers.response(res, null, { message: 'Token invalid' }, 403, 'Forbidden')
-      }
-    })
   }
+
+  // verifyAccessSeller: (req, res, next) => {
+  //   let token = req.headers.authorization
+  //   token = token.split(' ')[1]
+  //   jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+  //     if (!err) {
+  //       if (decoded.roleId != 1) {
+  //         return helpers.response(res, null, { message: 'You are not a seller' }, 403, 'Forbidden')
+  //       } else {
+  //         next()
+  //       }
+  //     } else {
+  //       return helpers.response(res, null, { message: 'Token invalid' }, 403, 'Forbidden')
+  //     }
+  //   })
+  // }
 }
